049b79a3b96078a4e15cc305af97ae4747d8ea47
Katarina Suu <katarina.suu@tktk.ee>
2023-03-22 18:42:34 +0200

    Logical Operators

--- Week3IfAndSwitch/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿//modulus - %
-
-int a = 24;
-int b = 8;
-
-int c = b % a; //+/-/* -/
-
-Console.WriteLine(c);
---
+++ Week5.LogicalOperators/Program.cs
@@ -0,0 +1,13 @@
+﻿//AND - &&
+
+//ture && ture --> true
+//ture && false --> false
+//false && ture --> false
+//false && false --> false
+
+// OR - || (pipes)
+
+//ture || ture --> true
+//ture || false --> true
+//false || ture --> true
+//false || false --> false
